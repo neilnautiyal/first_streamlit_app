9b21bad2b2d287a5056ee822bc734724ab6aa27d
neilnautiyal <75712709+neilnautiyal@users.noreply.github.com>
2023-07-31 12:37:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# nomralize 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output normalized
+streamlit.dataframe(fruityvice_normalized)
